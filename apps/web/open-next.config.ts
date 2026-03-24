1bfa6e0fe5084b7794d0d6977d3d9236ee478c5b
alexgutscher26 <workinbox69@gmail.com>
2026-03-23 23:07:25 -0500

    feat: Add `open-next.config.ts` to configure Cloudflare Node wrapper and Edge converter.

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -2,6 +2,17 @@ const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
     },
   },
 };
